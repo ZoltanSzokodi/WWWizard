041c7c145d317b8308b23da375c119a9cd479a25
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-04-16 15:33:51 +0200

    Logout, clear token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types';
 
 // Load user
@@ -78,4 +79,9 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
