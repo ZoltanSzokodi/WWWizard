11315581c43cc4fe2ba948ba4ab5f9a3cd9914aa
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-04-24 16:56:28 +0200

    List Education & Experience

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,8 +1,10 @@
-import React, { useEffect, Fragment } from 'react';
-import DashboardActions from './DashboardActions';
+import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
@@ -18,16 +20,25 @@ const Dashboard = ({
     <Fragment>
       <h1 className='large text-primary'>Dashboard</h1>
       <p className='lead'>
-        <i className='fas fa-user'></i>
-        Welcome {user && user.name}
+        <i className='fas fa-user' /> Welcome {user && user.name}
       </p>
-      {profile !== null ? (
+      {profile !== null &&
+      profile.experience !== undefined &&
+      profile.education !== undefined ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
+
+          <div className='my-2'>
+            <button className='btn btn-danger'>
+              <i className='fas fa-user-minus' /> Delete My Account
+            </button>
+          </div>
         </Fragment>
       ) : (
         <Fragment>
-          <p>You have not yet set up your profile</p>
+          <p>You have not yet setup a profile, please add some info</p>
           <Link to='/create-profile' className='btn btn-primary my-1'>
             Create Profile
           </Link>
