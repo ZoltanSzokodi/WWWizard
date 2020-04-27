652a2c4463a8854cd3f210739dfc7d136640609e
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-04-27 13:27:49 +0200

    Profile Components

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -5,6 +5,7 @@ import Login from '../auth/Login';
 import Alert from '../layout/Alert';
 import Dashboard from '../dashboard/Dashboard';
 import Profiles from '../profiles/Profiles';
+import Profile from '../profile/Profile';
 import ProfileForm from '../profile-forms/ProfileForm';
 import PrivateRoute from '../routing/PrivateRoute';
 import AddExperience from '../profile-forms/AddExperience';
@@ -18,6 +19,7 @@ const Routes = props => {
         <Route exact path='/register' component={Register} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/profiles' component={Profiles} />
+        <Route exact path='/profile/:id' component={Profile} />
         <PrivateRoute exact path='/dashboard' component={Dashboard} />
         <PrivateRoute exact path='/create-profile' component={ProfileForm} />
         <PrivateRoute exact path='/edit-profile' component={ProfileForm} />
